4e5a39d049193194f819b9f053d091d1c5e3bbf5
Jack Chao <924160539@qq.com>
2020-01-20 17:09:57 +0800

    格式化时间模块更改

--- src/utils/formateDate.js
+++ src/utils/formateDate.js
@@ -1,22 +1,12 @@
-// 时间格式化函数
+// 个位数字补零函数
+const formate = (time) => time = time < 10 ? '0' + time : time
+
+// 向外暴露时间格式化函数
 export default function formateDate(time) {
   if (!time) return ''
   let date = new Date(time)
-  let year = date.getFullYear()
-  let month = date.getMonth() + 1
-  let day = date.getDate()
-  let hours = date.getHours()
-  let minutes = date.getMinutes()
-  let seconds = date.getSeconds()
-
-  // 个位数字补零函数
-  const formate = (time) => time = time < 10 ? '0' + time : time
-
-  month = formate(month)
-  day = formate(day)
-  hours = formate(hours)
-  minutes = formate(minutes)
-  seconds = formate(seconds)
+  const timeArr = [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()]
+  const [year, month, day, hours, minutes, seconds] = timeArr.map(time => formate(time))
   // 返回一个格式为 YYYY-MM-DD hh-mm-ss 的时间字符串
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
-}
+}Y
